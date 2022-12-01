b3d16bcf35f12e62f3a3665a16950f6579bfdd3e
Gerrit Gutzeit <github@gerrit-gutzeit.de>
2022-12-02 00:46:05 +0100

    Update tailwind.config.js
    fixed color issue, smart not hard

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,40 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                blue: colors.sky,
-                gray: colors.black,
-                purple: '#313131',
+                blue: {
+                        100: '#c7c7c7',
+                        200: '#c7c7c7',
+                        300: '#c7c7c7',
+                        400: '#c7c7c7',
+                        500: '#c7c7c7',
+                        600: '#c7c7c7',
+                        700: '#c7c7c7',
+                        800: '#c7c7c7',
+                        900: '#c7c7c7',
+                      },
+                gray: {
+                        100: '#3f3f3f',
+                        200: '#3f3f3f',
+                        300: '#3f3f3f',
+                        400: '#3f3f3f',
+                        500: '#1c1c1c',
+                        600: '#1c1c1c',
+                        700: '#1c1c1c',
+                        800: '#1c1c1c',
+                        900: '#111111',
+                      },
+                
+                purple: {
+                        100: '#3d3d3d',
+                        200: '#3d3d3d',
+                        300: '#3d3d3d',
+                        400: '#3d3d3d',
+                        500: '#3d3d3d',
+                        600: '#3d3d3d',
+                        700: '#3d3d3d',
+                        800: '#272727',
+                        900: '#272727',
+                        },
             },
             fontFamily: {
                 merriweather: ['Merriweather', ...defaultTheme.fontFamily.serif],
